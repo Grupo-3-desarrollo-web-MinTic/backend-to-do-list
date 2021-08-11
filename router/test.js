3807e4542bbd6ecf4a23144597958f51069671f5
AndresMpa <andresprieto2703@gmail.com>
2021-08-11 14:43:11 -0500

    Prueba:
    He agregado algunas rutas de pruebas

--- router/test.js
+++ router/test.js
@@ -4,10 +4,14 @@ const routerx = require("express-promise-router");
 const router = routerx();
 
 /*Instacia + https + ( 'ruta', funcion )*/
-router.get("/send", test.send);
-router.get("/more", test.sendMore);
-router.get("/List", test.sendList);
-router.post("/login", test.login)
+router.get("/list", test.list);
+router.post("/login", test.login);
+router.get("/historic", test.listHistoric);
+router.post("/createUser", test.createUser);
+router.post("/createTask", test.createTask);
+router.post("/updateUser", test.updateUser);
+//router.post("/updateTask", test.updateTask);
+router.post("/deleteUser", test.deleteUser);
+router.post("/deleteTask", test.deleteTask);
 
 module.exports = router;
-
